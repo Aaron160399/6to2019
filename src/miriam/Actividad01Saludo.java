676d09be0e4df0098d261d3d358c1e5dc1eeee07
MiriHuesca <57206684+MiriHuesca@users.noreply.github.com>
2020-02-04 12:02:22 -0600

    Actividad01

--- src/miriam/Actividad01Saludo.java
+++ src/miriam/Actividad01Saludo.java
@@ -10,10 +10,10 @@ import aaron.*;
 
 /**
  *
- * @author Aaron
+ * @author Miriam
  */
 public class Actividad01Saludo {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("Miriam Hernández Huesca\n Ya entré a mi carpeta");
     }
 }
